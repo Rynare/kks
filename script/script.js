d1d2649f899479b0ae7c5d37aa381df47e9f3304
Rynare <rynare.of.legend@gmail.com>
2024-08-21 17:37:12 -0700

    finish

--- script/script.js
+++ script/script.js
@@ -3,6 +3,14 @@ const nav = document.querySelector("nav.navbar")
 document.addEventListener("DOMContentLoaded", () => {
     document.addEventListener("scroll", setNavScrolled)
     setNavScrolled()
+    AOS.init();
+    const navCollapse = nav.querySelector(".navbar-collapse");
+    navCollapse.addEventListener("show.bs.collapse", () => {
+        nav.classList.add("nav-showed");
+    });
+    navCollapse.addEventListener("hide.bs.collapse", () => {
+        nav.classList.remove("nav-showed");
+    });
 })
 
 function setNavScrolled() {
